3cd82310e5345365f5de8abad14a03ce0c684cd4
Jeff McKenzie <mackenzie.luceononuro@gmail.com>
2017-12-13 14:48:35 -0800

    Moved home page html to a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -13,17 +14,32 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
+        # The page and the browser show the to do text
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish The Test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # The user is able to enter a new item directly
+        text_input = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                text_input.get_attribute('placeholder', 'Enter a to-do item')
+        )
 
-        # The user types "Buy a real dinosaur fossil"
+        # The user types "Buy a T-Rex"
+        text_input.sendkeys('Buy a T-Rex')
 
         # When the user presses "enter" they see the new todo item saved
-        # 1: "Buy a real dinosaur fossil"
+        # 1: "Buy a T-Rex"
+        text_input.sendkeys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertTrue(
+            any(row.text == '1: Buy a T-Rex' for row in rows)
+        )
 
         # The user can still add items, and enters "Take over the world"
+        self.fail('Finish The Test!')
 
         # The page now shows both items
 
